80d72eef86af228ba5818f56d2fa0fa4ce80fc4a
benofben <ben.lackey@outlook.com>
2017-04-10 11:52:17 -0700

    parsing now

--- consolidate.py
+++ consolidate.py
@@ -1,5 +1,23 @@
 import os
+import csv
 
-for file in os.listdir("./reports"):
-    if file.endswith(".csv"):
-        print(os.path.join("./reports", file))
+def run():
+    filenames=get_filenames()
+
+    for filename in filenames:
+        process_file(filename)
+
+def get_filenames():
+    filenames=[]
+    for file in os.listdir("./reports"):
+        if file.endswith(".csv"):
+            filenames.append(os.path.join("./reports", file))
+    return filenames
+
+def process_file(filename):
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row)
+
+run()
