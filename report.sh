e785b241d7aa903d5173d12f20d38e3da0e82805
benofben <ben.lackey@outlook.com>
2017-04-10 09:46:22 -0700

    parameterize date

--- report.sh
+++ report.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 # You to be logged in as aws_partnership@couchbase.com with the AWS CLI to run this.
+
+# This report isn't available before 2015-01-26
+date=2015-01-26T00:00:00.000Z
+
 aws marketplacecommerceanalytics generate-data-set \
 --data-set-type daily_business_usage_by_instance_type \
---data-set-publication-date 2015-01-26T00:00:00.000Z \
+--data-set-publication-date $date \
 --role-name-arn arn:aws:iam::728631713266:role/MarketplaceCommerceAnalyticsRole \
 --destination-s3-bucket-name marketplacereporting \
 --sns-topic-arn arn:aws:sns:us-east-1:728631713266:marketplacereporting
