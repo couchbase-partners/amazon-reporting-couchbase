5bb2612d2f5684e7d4ecdfa53ac37fd2d21a560c
benofben <ben.lackey@outlook.com>
2017-09-27 18:29:55 -0700

    break out legacy revenue

--- consolidate.py
+++ consolidate.py
@@ -6,7 +6,7 @@ def run():
     filenames=get_filenames()
 
     for filename in filenames:
-        [hourly, byol, legacy, revenue] = process_file(filename)
+        [hourly, byol, legacy, revenue, legacyrevenue] = process_file(filename)
 
         #filenames look like daily_business_usage_by_instance_type_2015-04-02.csv
         date=filename[48:55] + '-1'
@@ -17,11 +17,13 @@ def run():
             usage[date]['byol']=0
             usage[date]['legacy']=0
             usage[date]['revenue']=0
+            usage[date]['legacyrevenue']=0
 
         usage[date]['hourly']+=hourly
         usage[date]['byol']+=byol
         usage[date]['legacy']+=legacy
         usage[date]['revenue']+=revenue
+        usage[date]['legacyrevenue']+=legacyrevenue
 
     write_usage(usage)
 
@@ -37,24 +39,27 @@ def process_file(filename):
     byol=0
     legacy=0
     revenue=0
+    legacyrevenue=0
 
     with open(filename) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             if 'Hourly' in row['Product Title']:
                 hourly+=int(row['Usage Units'])
+                revenue+=float(row['Estimated Revenue'])
             elif 'BYOL' in row['Product Title']:
                 byol+=int(row['Usage Units'])
+                revenue+=float(row['Estimated Revenue'])
             else:
                 legacy+=int(row['Usage Units'])
-            revenue+=float(row['Estimated Revenue'])
+                legacyrevenue+=float(row['Estimated Revenue'])
 
-    return [hourly, byol, legacy, revenue]
+    return [hourly, byol, legacy, revenue, legacyrevenue]
 
 def write_usage(usage):
-    print('Month, Hourly Pricing Usage, BYOL Usage, Legacy Usage, Total Usage, Revenue')
+    print('Month, Hourly Pricing Usage, BYOL Usage, Legacy Usage, Total Usage, Revenue, Legacy Revenue')
     for date in usage:
         total = usage[date]['hourly']+usage[date]['byol']+usage[date]['legacy']
-        print(date + ', ' + str(usage[date]['hourly']) + ', ' + str(usage[date]['byol']) + ', ' + str(usage[date]['legacy']) + ', ' + str(total) + ', ' + str(usage[date]['revenue']))
+        print(date + ', ' + str(usage[date]['hourly']) + ', ' + str(usage[date]['byol']) + ', ' + str(usage[date]['legacy']) + ', ' + str(total) + ', ' + str(usage[date]['revenue']) + ', ' + str(usage[date]['legacyrevenue']))
 
 run()
